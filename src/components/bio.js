07ac51b13e1b1acec50bae0d7bbc48c1e45d48c6
mark146 <tkdtkdqja@naver.com>
2019-03-23 18:29:06 +0900

    Update bio.js

--- src/components/bio.js
+++ src/components/bio.js
@@ -38,12 +38,7 @@ function Bio() {
               }}
             />
             <p>
-              Written by <strong>{author}</strong> who lives and works in San
-              Francisco building useful things.
-              {` `}
-              <a href={`https://twitter.com/${social.twitter}`}>
-                You should follow him on Twitter
-              </a>
+              <strong>{author}</strong> History
             </p>
           </div>
         )
@@ -54,7 +49,7 @@ function Bio() {
 
 const bioQuery = graphql`
   query BioQuery {
-    avatar: file(absolutePath: { regex: "/profile-pic.jpg/" }) {
+    avatar: file(absolutePath: { regex: "/wolf.jpg/" }) {
       childImageSharp {
         fixed(width: 50, height: 50) {
           ...GatsbyImageSharpFixed
