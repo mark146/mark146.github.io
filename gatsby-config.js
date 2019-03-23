272bec4c561fe4c823d2fd39f1797c2374b1a9f1
mark146 <tkdtkdqja@naver.com>
2019-03-23 17:54:41 +0900

    test
    blog test

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
-    author: `Kyle Mathews`,
+    title: `기술 블로그`,
+    author: `Jin`,
     description: `A starter blog demonstrating what Gatsby can do.`,
     siteUrl: `https://gatsby-starter-blog-demo.netlify.com/`,
     social: {
